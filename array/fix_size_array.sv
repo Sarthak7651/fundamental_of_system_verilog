b367a66bccdc97a4377a717494b7bae6a6951e4c
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-08-25 17:43:20 +0530

    Create fix_size_array.sv

---
+++ array/fix_size_array.sv
@@ -0,0 +1,21 @@
+module array;
+  
+  bit arr [7:0]; //in verilog
+  bit arr1 [8]; //in sv 8 element 
+  bit arr2 []={1,0,1,1};//initialisation of array
+  
+  initial 
+    begin
+      $display ("size of arr1 : %0d",$size(arr1));
+      $display ("size of arr2 : %0d",$size(arr2));
+    end
+  
+  initial 
+    begin
+      $display ("value of first element : %0d", arr1[0]);
+      arr1[1]=1; //to update the vaue of an array
+      $display ("value of first element : %0d",arr1[1]);
+      
+      $display ("value of all element of arr2 : %0p",arr2);
+    end
+endmodule
