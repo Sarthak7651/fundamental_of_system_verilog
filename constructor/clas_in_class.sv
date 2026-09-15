dbbe8bfa8556ca8317f6ebe79226fc5c03df9ded
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-09-15 11:20:27 +0530

    Update clas_in_class.sv

--- constructor/clas_in_class.sv
+++ constructor/clas_in_class.sv
@@ -27,3 +27,42 @@ module tb;
     s.f1.display();
   end
 endmodule
+
+
+//scope of data members 
+class first;
+  local int data=35;
+  task set (input int data);
+    this.data=data;
+  endtask
+  
+  function int get();
+    return data;
+  endfunction
+  
+  task display();
+    $display("value of data is %0d",data);
+  endtask
+
+endclass
+
+class second;
+  first f1;
+  function new();
+    f1=new();
+  endfunction
+endclass
+
+module tb;
+  second s;
+  initial begin
+    s=new();
+    s.f1.set(123);
+    s.f1.display();
+    //$display("value of data is: %0d",s.f1.get());
+  end
+endmodule
+
+/*Getter and setter methods: Bcz data is restricted the code provides public subroutines(set and get) to securely modify
+and read its value from outside the class bounds.
+class composition: the second class contain and instance of first class,establishing a structural hirerarhical relationship*/
