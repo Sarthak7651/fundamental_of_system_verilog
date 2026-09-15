1927817939829cb3c87598739fe21db790ac1980
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-09-15 10:49:01 +0530

    Update clas_in_class.sv

--- constructor/clas_in_class.sv
+++ constructor/clas_in_class.sv
@@ -1,6 +1,9 @@
-/*class composition - known as "has-a" relationship,where one class contain an instance of another class */
+/*class composition - known as "has-a" relationship,where one class contain an instance of another class.
+by making "local" int data,this variable becomes completely hidden from outside classes or module. it can only be 
+read or modified by task and function or methods written inside the first class. */
 
 class first;
+  //local int data=35;
   int data=35;
   task display();
     $display("value of data is %0d",data);
