a3e360fd907e2cd00456088aaac2e0a19de42701
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-09-15 12:06:14 +0530

    Update shallow_copy.sv

--- copying_objects/shallow_copy.sv
+++ copying_objects/shallow_copy.sv
@@ -25,3 +25,32 @@ p1 ─────────►│   data = 24  │
   end
 endmodule
     
+
+
+//another example of shallow copy
+class first;
+  int data=12;
+endclass
+
+class second;
+  int ds=34;
+  first f1;
+  function new();
+    f1=new();
+  endfunction
+endclass
+
+module tb;
+  second s1,s2;
+  initial begin
+    s1=new();
+    s1.ds=45;
+    s2=new s1;//Create a new object s2 by copying the properties of object s1
+    //s2 = s1; //true syntax for shallow copy
+    $display("value of ds:%0d",s2.ds);
+    s2.ds=78;
+    $display("value of data:%0d",s1.ds);
+    s2.f1.data=56;
+    $display("value of data:%0d",s1.f1.data);
+  end
+endmodule
