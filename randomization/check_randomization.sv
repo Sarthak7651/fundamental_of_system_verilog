a8e109fa4073ff3a7b133fafc128180b639ba87e
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-09-17 12:33:17 +0530

    Update check_randomization.sv

--- randomization/check_randomization.sv
+++ randomization/check_randomization.sv
@@ -22,3 +22,25 @@ module tb;
   end
 endmodule
 
+class generator;
+  rand bit [ 3:0] a,b;//randc
+  bit[3:0] y;
+endclass
+
+module tb;
+  generator g;
+  int i = 0;
+  int status=0;
+  initial begin
+    g=new();
+    for(i=0;i<10;i=i+1) begin
+      g.randomize();
+      $display("value of a : %0d and b : %0d",g.a,g.b);
+    end
+  end
+endmodule
+
+module dut(
+  input [3:0]a,b,
+  output [3:0]y);
+endmodule
