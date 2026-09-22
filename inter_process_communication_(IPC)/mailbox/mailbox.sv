fe12806718b70075376231bd442e2db00897dfa4
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-09-22 18:46:10 +0530

    Update mailbox.sv

--- inter_process_communication_(IPC)/mailbox/mailbox.sv
+++ inter_process_communication_(IPC)/mailbox/mailbox.sv
@@ -1,1 +1,56 @@
-bdj
+class generator;
+int data=12;
+mailbox mbx;
+
+
+function new (mailbox mbx);
+this.mbx=mbx;
+endfunction
+
+
+task run();
+mbx.put(data);
+$display("[GEN]:data send from gen :%0d",data);
+endtask
+endclass
+
+class driver;
+mailbox mbx;
+int data;
+
+
+function new (mailbox mbx);
+this.mbx=mbx;
+endfunction
+
+
+task run();
+//mbx.get(data);
+mbx.peek(data);
+$display("[DVR]:data rcvd :%0d",data);
+endtask
+endclass
+
+module tb;
+generator gen;
+driver drv;
+mailbox mbx;
+
+initial begin
+/*
+gen=new();
+drv=new();
+mbx=new();
+*/
+
+mbx=new();
+gen=new(mbx);
+drv=new(mbx);
+
+
+gen.mbx=mbx;
+//drv.mbx=mbx;
+gen.run();
+drv.run();
+end
+endmodule
