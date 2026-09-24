91ddfab3688dea1cde41b858613c7e33e5a4968b
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-09-24 13:27:32 +0530

    Update blocking_nonblocking_signal.sv

--- interfaces/blocking_nonblocking_signal.sv
+++ interfaces/blocking_nonblocking_signal.sv
@@ -1,1 +1,39 @@
+//design code
+module add(
+input [3:0] a,b,
+input clk,
+output reg [4:0] sum);
+always @(posedge clk)
+begin
+sum <= a+b;
+end
+endmodule
 
+//interface
+interface add_if;
+logic [3:0]a;
+logic [3:0]b;
+logic [4:0]sum;
+logic clk;
+endinterface
+
+module tb;
+add_if aif();
+add dut (.a(aif.a),.b(aif.b),.clk(aif.clk),.sum(aif.sum));
+ 
+initial begin
+$monitor("value of clk:%0b , a:%0d , b:%0d and sum:%0d",aif.clk,aif.a,aif.b,aif.sum);
+aif.clk=0;
+end
+always #10 aif.clk=~aif.clk;
+initial begin
+aif.a=1;
+aif.b=5;
+#22;
+aif.a=3;
+#20;
+aif.a=5;
+#8;
+aif.a=4;
+end
+endmodule
