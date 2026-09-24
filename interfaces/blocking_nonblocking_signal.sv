e43af44bbd34bec444729090d076f54578bba4d3
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-09-24 13:35:41 +0530

    Update blocking_nonblocking_signal.sv

--- interfaces/blocking_nonblocking_signal.sv
+++ interfaces/blocking_nonblocking_signal.sv
@@ -37,3 +37,35 @@ aif.a=5;
 aif.a=4;
 end
 endmodule
+
+//by using repeat block
+module tb;
+add_if aif();
+add dut (.a(aif.a),.b(aif.b),.clk(aif.clk),.sum(aif.sum));
+ 
+initial begin
+$monitor("value of clk:%0b , a:%0d , b:%0d and sum:%0d",aif.clk,aif.a,aif.b,aif.sum);
+aif.clk=0;
+end
+always #10 aif.clk=~aif.clk;
+initial begin
+//repeat (3) begin
+aif.a=1;
+aif.b=5;
+//#22;
+repeat (3) @(posedge aif.clk);
+aif.a=3;
+//#20;
+repeat (3) @(posedge aif.clk);
+aif.a=5;
+//#8;
+repeat (3) @(posedge aif.clk);
+aif.a=4;
+end
+initial begin
+$dumpfile ("interface.vcd");
+$dumpvars;
+#100;
+$finish;
+end
+endmodule
