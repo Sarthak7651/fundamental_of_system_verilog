bf56035e2219d0a0d302e3dba600983c4a452bc9
Sarthak Pandey <pandey0908sarthak@gmail.com>
2026-09-25 11:38:17 +0530

    Create adding_monitor_scoreboard.sv

---
+++ error_injection/adding_monitor_scoreboard.sv
@@ -0,0 +1,114 @@
+//design code
+module add(
+input [3:0] a,b,
+output reg [4:0] sum,
+input clk);
+always @(posedge clk)
+begin
+sum <= a+b;
+end
+endmodule
+
+class transaction;
+randc bit [3:0] a;
+randc bit [3:0] b;
+bit [4:0] sum;
+
+function void display();
+$display ("a:%0d | b:%0d | sum:%0d",a,b,sum);
+endfunction
+endclass
+
+interface add_if;
+logic [3:0] a;
+logic [3:0] b;
+logic [4:0] sum;
+logic clk;
+endinterface
+
+
+//monitor
+class monitor;
+mailbox #(transaction) mbx;
+transaction trans;
+virtual add_if aif;
+
+function new(mailbox #(transaction) mbx);
+this.mbx = mbx;
+endfunction
+
+task run();
+trans = new();
+
+forever begin;
+repeat(2) @(posedge aif.clk);
+trans.a = aif.a;
+trans.b = aif.b;
+trans.sum = aif.sum;
+$display("[MON] : Data sent to scoreboard");
+trans.display();
+mbx.put(trans);
+end
+endtask
+endclass
+
+//scoreboard
+class scoreboard;
+mailbox #(transaction) mbx;
+transaction trans;
+
+function new (mailbox #(transaction) mbx);
+this.mbx = mbx;
+endfunction
+
+task run();
+forever begin
+mbx.get(trans);
+$display ("[SC0] : Data RCVD from monitor");
+trans.display();
+#20;
+end
+endtask
+endclass
+
+module tb;
+add_if aif();
+monitor mon;
+scoreboard sco;
+mailbox #(transaction) mbx;
+add dut (.a(aif.a),.b(aif.b),.sum(aif.sum),.clk(aif.clk));
+
+initial begin
+aif.clk <= 0;
+end
+
+always #10 aif.clk = ~aif.clk;
+initial begin
+for(int i=0; i<20; i=i+1) begin
+repeat (2) @(posedge aif.clk);
+aif.a <= $urandom_range (0,15);
+aif.b <= $urandom_range (0,15);
+end
+end
+
+initial begin
+mbx = new();
+mon = new(mbx);
+sco = new(mbx);
+mon.aif=aif;
+end
+
+initial begin
+fork
+mon.run();
+sco.run();
+join
+end
+
+initial begin
+$dumpfile("dump.vcd");
+$dumpvars;
+#450;
+$finish;
+end
+endmodule
